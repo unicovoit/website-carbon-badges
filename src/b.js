9e0a35848d3cf82ed001b5578d53afa6ed3e3f59
Jerome Toole <jerome@wholegraindigital.com>
2020-02-05 16:26:34 +0000

    support caching in localstorage

--- src/b.js
+++ src/b.js
@@ -1,27 +1,67 @@
-let $q = (selector) => document.querySelector(selector);
+const $q = (selector) => document.querySelector(selector);
+const url = encodeURIComponent(window.location.href)
 
-let s = `{{css}}`;
+const newRequest = function (render = true) {
+    // Run the API request because there is no cached result available
+    fetch('https://api.websitecarbon.com/b?url=' + url)
+        .then(function (r) {
+            if (!r.ok) {
+                throw Error(r);
+            }
+            return r.json();
+        })
 
+        .then(function (r) {
+            if (render) {
+                renderResult(r)
+            }
+
+            // Save the result into localStorage with a timestamp
+            r.t = new Date().getTime()
+            localStorage.setItem('wcb_'+url, JSON.stringify(r))
+        })
+
+        // Handle error responses
+        .catch(function (e) {
+            $q('#cbg').innerHTML = 'No Result';
+            console.log(e);
+            localStorage.removeItem('wcb_'+url)
+        })
+}
+
+// Get the CSS and add it to the DOM. The placeholder will be filled by gulp build
+const s = '{{css}}';
 $q('#cb').insertAdjacentHTML('beforeEnd',s)
 
-let m = `<div id="cbp"><p id="cbg">Measuring CO<sub>2</sub>&hellip;</p><a target="_blank" rel="noopener" href="https://websitecarbon.com">Website Carbon</a></div>
-    <p id="cb2"></p>`
+// Add the badge markup
+$q('#cb').insertAdjacentHTML('beforeEnd', '<div id="cbp"><p id="cbg">Measuring CO<sub>2</sub>&hellip;</p><a target="_blank" rel="noopener" href="https://websitecarbon.com">Website Carbon</a></div>');
 
-$q('#cb').insertAdjacentHTML('beforeEnd',m);
+const renderResult = function(r) {
+    $q('#cbg').innerHTML = r.c + 'g of CO<sub>2</sub>/view'
+    $q('#cb').insertAdjacentHTML('beforeEnd', '<p id="cb2">Cleaner than ' + r.p + '% of pages tested</p>')
+}
 
-fetch('https://api.websitecarbon.com/b?url=' + encodeURIComponent(window.location.href))
-    .then(function(r) {
-        if (!r.ok) {
-            throw Error(r);
+// Get result if it's saved
+let cr = localStorage.getItem('wcb_'+url)
+const t = new Date().getTime()
+
+// If there is a cached response, use it
+if (cr) {
+    const r = JSON.parse(cr)
+
+    // If time since response was cached is over a week, then don't use cached response and make a call to the API instead
+    if ((t - r.t) > (604800000)) {
+        newRequest()
+    } else {
+        renderResult(r)
+
+        // If time since response was cached is over a day, then make a new request and update the cached result in the background
+        if ((t - r.t) > (86400000)) {
+            newRequest(false)
         }
-        return r.json();
-    })
-    .then(function (r) {
-        $q('#cbg').innerHTML = r.c + 'g of CO<sub>2</sub>/view'
-        $q('#cb2').innerHTML = 'Cleaner than ' + r.p + '% of pages tested'
-    })
-    .catch(function (e) {
-        $q('#cbg').innerHTML = 'No Result';
-        console.log(e);
-    })
+    }
 
+// If no cached response, then fetch from API
+} else {
+    newRequest()
+}
